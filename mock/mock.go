161891160828da7f5c6d2f0b99d417fbef037e1f
Sam Allen <samallen659@gmail.com>
2023-09-18 19:40:14 +0100

    adds mock

---
+++ mock/mock.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(w io.Writer, sleeper Sleeper) {
+	for i := 3; i > 0; i-- {
+		fmt.Fprintln(w, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(w, finalWord)
+}
+
+func main() {
+	Countdown(os.Stdout, &DefaultSleeper{})
+}
