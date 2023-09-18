61fee262ab93726e2cdeb4d563e0413ae6428351
Sam Allen <samallen659@gmail.com>
2023-09-18 13:58:27 +0100

    adds triangle shape and tests

--- structsMethodsInterfaces/shapes.go
+++ structsMethodsInterfaces/shapes.go
@@ -15,6 +15,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Width  float64
+	Height float64
+}
+
 func Perimeter(rec Rectangle) float64 {
 	return 2 * (rec.Width + rec.Height)
 }
@@ -25,4 +30,8 @@ func (r Rectangle) Area() float64 {
 
 func (c Circle) Area() float64 {
 	return math.Pi * math.Pow(c.Radius, 2)
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Height * t.Width) * 0.5
 }
